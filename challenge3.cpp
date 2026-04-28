8d464b5c397f15a014cbcb2b0dff2de55ec07072
Laone Makeketa <laonemakeketa@gmail.com>
2026-04-28 17:55:02 +0200

    Add files via upload

---
+++ challenge3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//Convert a double value into an integer.   
+
+using namespace std;
+
+int main(){
+    double double_value = 70.986;
+    int int_value ;
+    int_value = (int) double_value;
+    cout << "Value: " << int_value;
+    return 0;
+}
