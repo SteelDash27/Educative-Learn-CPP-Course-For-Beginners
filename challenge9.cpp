339dac3c4c3b8d40f392ee7cdfc365f9df7b3e74
Laone Makeketa <laonemakeketa@gmail.com>
2026-05-05 12:52:30 +0200

    Add files via upload

---
+++ challenge9.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+/*In this challenge, you are given a weekday number. 
+Your task is to display 
+the corresponding day name to the
+ console using the switch statement.
+*/
+using namespace std;
+
+int main(){
+    int dayofweek;
+    cout << "Enter in a numbe from 1 - 7" << endl;
+    cin >> dayofweek;
+
+    switch(dayofweek){
+        case 1 :
+        cout << "Monday";
+        break;
+        case 2 :
+        cout << "Tuesday";
+        break;
+        case 3:
+        cout << "Wednesday";
+        break;
+        case 4 :
+        cout << "Thursday";
+        break;
+        case 5 :
+        cout << "Friday";
+        break;
+        case 6 :
+        cout << "Saturday";
+        break;
+        case 7 :
+        cout << "Sunday";
+        break;
+    }
+    return 0;
+}
