dcb0d4a55c2b67280d4b684c523922176a8591f2
Laone Makeketa <laonemakeketa@gmail.com>
2026-05-07 14:30:04 +0200

    Refactor challenge to display day of the week

--- Challenge 9: Display Day of the Week.cpp
+++ Challenge 9: Display Day of the Week.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main(){
     int dayofweek;
+ 
     cout << "Enter in a numbe from 1 - 7" << endl;
     cin >> dayofweek;
 
@@ -15,24 +16,31 @@ int main(){
         case 1 :
         cout << "Monday";
         break;
+     
         case 2 :
         cout << "Tuesday";
         break;
+     
         case 3:
         cout << "Wednesday";
         break;
+     
         case 4 :
         cout << "Thursday";
         break;
+     
         case 5 :
         cout << "Friday";
         break;
+     
         case 6 :
         cout << "Saturday";
         break;
+     
         case 7 :
         cout << "Sunday";
         break;
+     
         default:
         cout << "Invalid Output";
     }
