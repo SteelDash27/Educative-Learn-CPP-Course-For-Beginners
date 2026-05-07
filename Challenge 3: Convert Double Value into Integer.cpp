b011ef8f03c6ff3c4099b775e933849c6047a27c
Laone Makeketa <laonemakeketa@gmail.com>
2026-05-07 13:17:37 +0200

    Refactor code formatting in conversion example

--- Challenge 3: Convert Double Value into Integer.cpp
+++ Challenge 3: Convert Double Value into Integer.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 
 int main(){
+    
     double double_value = 70.986;
     int int_value ;
+    
     int_value = (int) double_value;
     cout << "Value: " << int_value;
+    
     return 0;
 }
