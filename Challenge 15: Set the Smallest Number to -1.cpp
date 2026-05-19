3a4227262f83a502779ceb95b72de2be0b7f5a24
Laone Makeketa <laonemakeketa@gmail.com>
2026-05-19 15:55:14 +0200

    Add comments to minimum function
    Added comments to clarify the function's purpose and behavior.

--- Challenge 15: Set the Smallest Number to -1.cpp
+++ Challenge 15: Set the Smallest Number to -1.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
 using namespace std;
-
+/*
+Your task is to write a function minimum. In the function parameter, you will pass 
+two values of type int by reference, and function will set the smallest number of the two to -1.
+Your function should: Compare the values of number1 and number2 and set 
+the variable with the smallest value to -1;
+If values of both number1 and number2 are equal, then set both to -1.
+*/
 void minimum(int &num1,int &num2);
 
 int main(){
